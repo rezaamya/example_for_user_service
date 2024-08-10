e5e0466447e4f3455f8c81dcab669381da922fb6
Reza Amya <rezaamya@gmail.com>
2024-08-11 02:32:04 +0330

    feat: run `migration.sql` on app start
    `pg` package installed and provided globally with `PG` alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,55 @@
-import { Module } from "@nestjs/common";
-import { UserModule } from "./modules/user/user.module";
-import { ConfigModule } from "@nestjs/config";
-import appConfig from "./configs/app.config";
-import { configValidator } from "./configs/validator";
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import { join } from 'path';
+import { Pool } from 'pg';
+import { UserModule } from './modules/user/user.module';
+import appConfig from './configs/app.config';
+import { configValidator } from './configs/validator';
+import postgresConfig, { IPostgresConfig } from './configs/postgres.config';
 
+@Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [appConfig],
-      validate: configValidator
+      load: [appConfig, postgresConfig],
+      validate: configValidator,
     }),
-    UserModule],
+    UserModule,
+  ],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: 'PG',
+      useFactory: async (configService: ConfigService) => {
+        try {
+          const postgresConfig = configService.get<IPostgresConfig>('postgres');
+          const pool = new Pool({
+            host: postgresConfig.host,
+            user: postgresConfig.user,
+            password: postgresConfig.password,
+            database: postgresConfig.database_name,
+            port: postgresConfig.port,
+          });
+
+          const poolClient = await pool.connect();
+
+          const sql = fs
+            .readFileSync(join(__dirname, 'migration.sql'))
+            .toString();
+          await poolClient.query(sql);
+          poolClient.release();
+
+          return pool;
+        } catch (e) {
+          throw e;
+        }
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: ['PG'],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/migration.sql
@@ -0,0 +1,64 @@
+CREATE TABLE IF NOT EXISTS migration
+(
+    meta_key   VARCHAR(100) PRIMARY KEY,
+    meta_value VARCHAR(255) DEFAULT NULL
+);
+
+INSERT INTO migration (meta_key, meta_value)
+VALUES ('version', '000.000.000')
+ON CONFLICT
+    (meta_key)
+    DO NOTHING;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+DO
+$$
+    DECLARE
+        version        VARCHAR(20) := (SELECT meta_value
+                                       FROM migration
+                                       WHERE meta_key = 'version');
+        DECLARE v1_0_0 VARCHAR(20) := '001.000.000';
+    BEGIN
+
+        -- v1.0.0
+        IF version < v1_0_0 THEN
+            if not exists(select 1 from pg_type where typname = 'user_status') then
+                create type user_status as enum ('active', 'blocked');
+            end if;
+
+            if not exists(select 1 from pg_type where typname = 'user_registered_by') then
+                create type user_registered_by as enum ('email', 'google');
+            end if;
+
+            create table users
+            (
+                "userId"         serial                       not null,
+                name             varchar                      not null,
+                email            varchar                      not null,
+                "sanitizedEmail" varchar                      not null,
+                password         varchar,
+                status           user_status default 'active' not null,
+                "registeredBy"   user_registered_by           not null,
+                "registeredAt"   timestamp   default now()    not null,
+                "updatedAt"      timestamp   default now()    not null
+            );
+
+            create unique index users_sanitizedEmail_uindex
+                on users ("sanitizedEmail");
+
+            create index users_userId_index
+                on users ("userId");
+
+            alter table users
+                add constraint users_pk
+                    primary key ("userId");
+
+            UPDATE migration
+            SET meta_value = v1_0_0
+            WHERE meta_key = 'version';
+        END IF;
+
+    END
+$$;
+
