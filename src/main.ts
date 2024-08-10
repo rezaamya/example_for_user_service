21512d47d4538268d7538526991a3fe0b54c485f
Reza Amya <rezaamya@gmail.com>
2024-08-11 02:25:08 +0330

    feat: enable global validation and transformation for incoming requests

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { IAppConfig } from "./configs/app.config";
+import { IAppConfig } from './configs/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const configService = app.get<ConfigService>(ConfigService);
   const appConfig = configService.get<IAppConfig>('app');
